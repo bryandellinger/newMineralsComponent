3ebb4f3eb24d2ca974b7399fb3fae2a790aedfff
bryandellinger <dellingerfam5@gmail.com>
2021-06-30 21:44:14 -0400

    Create insertpettypes.sql

---
+++ insertpettypes.sql
@@ -0,0 +1,17 @@
+Insert into PetTypes (PetTypeName) VALUES ('Dog');
+Insert into PetTypes (PetTypeName) VALUES ('Cat');
+Insert into PetTypes (PetTypeName) VALUES ('Bird');
+Insert into PetTypes (PetTypeName) VALUES ('Fish');
+Insert into PetTypes (PetTypeName) VALUES ('Gerbil');
+Insert into PetTypes (PetTypeName) VALUES ('Rabbit');
+Insert into PetTypes (PetTypeName) VALUES ('Turtle');
+Insert into PetTypes (PetTypeName) VALUES ('Snake');
+Insert into PetTypes (PetTypeName) VALUES ('Hamster');
+Insert into PetTypes (PetTypeName) VALUES ('Horse');
+Insert into PetTypes (PetTypeName) VALUES ('Ferret');
+Insert into PetTypes (PetTypeName) VALUES ('Guinea Pig'); 
+Insert into PetTypes (PetTypeName) VALUES ('Frog');
+Insert into PetTypes (PetTypeName) VALUES ('Llama');
+Insert into PetTypes (PetTypeName) VALUES ('Spider');
+Insert into PetTypes (PetTypeName) VALUES ('Lizard');
+Insert into PetTypes (PetTypeName) VALUES ('Hermit Crab');
